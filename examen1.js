f879b236d41d3c885b9198e852e68215e6a2c4fb
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-16 15:07:06 -0600

    Create examen1.js

---
+++ examen1.js
@@ -0,0 +1,27 @@
+/*
+
+1.Es la palabra clave que necesita una función para devolver una salida:
+
+return
+
+2.Ademas de la sintaxis ¿Cuál es la principal diferencia entre una función declarativa y una expresión de función?
+
+Podemos llamar una función declarativa antes de ser declarada
+
+3.En JavaScript es posible declara e inicialar una variable en la misma línea:
+
+Verdadero
+
+4.Son los tipos de datos primitivos que maneja JavaScript:
+
+String, Number, Boolean, Null y Undefined
+
+5.¿En qué tipo de proyectos podemos utilizar JavaScript?
+
+Frontend, Backend, aplicaciones móviles, aplicaciones de escritorio y IoT
+
+6.¿Por qué nace JavaScript?
+
+Para darle interacción y dinamismo a los sitios web
+
+*/
