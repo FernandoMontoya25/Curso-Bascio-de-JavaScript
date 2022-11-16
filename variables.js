c8d5eca947a1765aca0ecfcfd27298376924e048
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-16 08:50:27 -0600

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,23 @@
+
+/* Variable es un lugar en memoria
+Es una representacion de algun lugar en la memoria para guardar datos
+*/
+var nombre = "Fernando"
+nombre //llamarla
+
+var edad; // Declarar
+edad = 23; //Inicializar
+// Mostrarla en consola
+console.log(nombre) //Fernando
+
+// Variable tipo objeto
+var persona = {
+  nombre: "Fernando",
+  edad: 30
+}
+// Acceder a un valor especifico en nuestro array(objeto)
+var elementos = ["Computadora","Celular"];
+console.log(elementos[1]);
+// Nos entregara el valor de celular
+
+  
