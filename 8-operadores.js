0311a0f2656e88b7820f2f7241d9065327a73352
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-17 15:53:10 -0600

    Create 8-operadores.js

---
+++ 8-operadores.js
@@ -0,0 +1,37 @@
+
+// de operación 
+
+3 + 2
+50 - 10
+10 * 3
+20 / 2
+
+"Fernando" + " Montoya" = "Fernando Montoya"
+
+// Operadores Lógicos 
+
+!false // operador not (no)
+
+= // asignacion
+   
+3 == '3' // comparacion
+
+3 === '3' // es estrictamente igual 
+
+5 < 3, 5 <= 3, 5 > 3, 5 >= 3 // menor, menor o igual, mayo, mayor o igual 
+
+a && b // operador and (y), este operador genera una valicación siempe y cuando ambas variables sean verdad, "var 1 y var 2"
+
+true || false // operador or, validacion cuando este o este son verdaderos
+
+/* ================================================================ */ 
+
+// Operador que solo se pueden utilizar con variables 
+
+var edad = 40;
+
+edad++;  // operador de incremento por 1
+edad += 2; 
+
+edad;
+/* https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Expressions_and_Operators */
