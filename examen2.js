1f5bf00c3afcc3bc74c1b52a335a960b77f26d2b
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-17 17:50:25 -0600

    Create examen2.js

---
+++ examen2.js
@@ -0,0 +1,24 @@
+/*
+
+1. ¿Qué diferencia hay entre comparar con “==” y “===”?
+
+“==” compara valores y “===” compara valores y tipo de dato
+
+2. ¿Cuál es la naturaleza de los valores booleanos?
+
+True y false
+
+3. ¿En qué consiste la coerción explicita?
+
+En obligar a cambiar el tipo de una variable a otra
+
+4. ¿Qué valor se asigna por defecto a las variables cuando caen en hoisting?
+
+Undefined
+
+5. En el scope local de una función puedo usar variables: 
+
+Variables del scope global y locales de esa funciona
+
+
+*/
