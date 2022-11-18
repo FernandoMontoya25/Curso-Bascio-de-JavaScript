eb0fb57764c6a4825e74bede84db6deb8118c02a
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-17 18:23:11 -0600

    Create 9-condiciona-if.js

---
+++ 9-condiciona-if.js
@@ -0,0 +1,25 @@
+
+
+/* Las condicionales son estructuras de control que te permiten evaluar diferentes expresiones 
+y realizar determinadas acciones en JavaScript*/
+
+// Ejemplo 1
+
+var edad = 18;
+if(edad < 18){
+  console.log("Aun no puedes votar");
+} else if(edad === 18){
+  console.log("Sera tu primera votacion");
+}else{
+  console.log("Puedes votar otra vez");
+}
+
+// Operador ternario
+
+// condition ? true : false
+
+var numero = 1;
+var resultado = numero === 1 ? "Si soy un 1" : "No soy uno"
+
+
+ 
