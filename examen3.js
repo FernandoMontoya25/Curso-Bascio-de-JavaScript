c6495aa14a74c84e6e63fe52b56bac2937545656
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-18 11:21:12 -0600

    Create examen3.js

---
+++ examen3.js
@@ -0,0 +1,28 @@
+/*
+1. ¿Qué pasa si no coloco la sentencia “break” en un switch?
+
+  Se validan todos los casos como verdaderos y se realizan las acciones asignadas a cada uno
+
+2. ¿Que retorna el siguiente código? */
+  
+  if(false === !true){
+    console.log(false == true)
+  } else {
+    console.log(true === !false)
+  }
+      
+falso
+
+/*
+3. ¿En qué casos es conveniente utilizar “switch”?
+
+  Cuando tenemos múltiples casos a evaluar y evitar el uso de múltiples “if else”
+
+4. ¿Para qué nos sirve el operador ternario “?”?
+
+  Para definir una condición el resultado cuando se cumple y cuando no se cumple en una sola línea
+*/
+
+
+
+  
