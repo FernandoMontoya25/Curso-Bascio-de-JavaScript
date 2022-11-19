4a977c1d14daf36d2deedcec98558538f15b1afe
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-18 21:18:24 -0600

    Create 14-objetos.js

---
+++ 14-objetos.js
@@ -0,0 +1,27 @@
+/* Un objeto es una estructura de datos que permite almacenar valores mediante 
+propiedad - valor a traves de la sintaxis de llaves ( {} ) y separados por comas. */
+
+// Ejemplo de objeto
+var miAuto = {
+  marca: "Toyota", 
+  modelo: "corola", 
+  annio: "2020", 
+  detallesDelAuto: function(){
+    console.log(`Auto ${this.modelo} ${this.annio}`)
+  };
+
+// this: Hace referencia a su propiedad padre por ejemplo habla que quiere el valor 
+// que tiene la variable modelo de su padre o el año
+  
+  
+// Acceder a algo especifico
+console.log(miAuto.marca)
+// Como añadir propiedades a un objeto 
+miAuto.color = "rojo"; // y ya con esto lo tiene agregado
+// Como modificar propiedades de un objeto
+miAuto.marca = "Ford"
+// Como eliminar propiedades de un objeto
+delete miAuto.marca
+
+
+ 
