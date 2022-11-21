5d86de9328f8e1f22b40ef5a19ad64765ccef781
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2022-11-21 07:07:06 -0600

    Create examenFinal.js

---
+++ examenFinal.js
@@ -0,0 +1,83 @@
+
+/*1.	¿Qué es un array?
+
+Es una estructura de datos de tipo objeto
+
+2.	¿Qué resultado podría esperar de esta validación? 4 == “4”
+
+True
+
+3.	¿Es correcta la siguiente sentencia? */
+
+switch (false){
+  case false: console.log("Soy falso")
+  case true: console.log("soy verdadero")
+
+/*NO
+
+4.	¿Cuáles son los tipos de scope que tenemos?
+
+Scope global y Scope local
+
+5.	¿Qué resultado podría esperar de esta validación? 1 = “1”
+
+Syntax Error
+
+6.	¿Qué es una variable?
+
+Es la representación de un espacio en memoria
+
+7.	Que podemos esperar al llamar al método. pop();?
+
+Borrar el ultimo elemento de mi array.
+
+8.	¿Que resultado podría esperar de esta validación? 6 === “6”
+
+False
+
+9.	¿Qué método llamaríamos para saber el index de un elemento de mi array?
+
+indexOf()
+
+10.	¿Como se le conoce a la siguiente sentencia? */
+
+    var nombre;
+// Declarar una variable
+
+/* 11.	¿Por qué decimos que JavaScript es un lenguaje dinámico?
+
+Por que es un lenguaje que correen tiempo de ejecución
+
+12.	¿Como se le llama al siguiente ejemplo de funciones */ 
+
+    function myfunction() {
+  ...
+}
+//Función declarativa
+
+//13.	¿Como podemos crear notas o comentarios en nuestro JavaScript?
+
+//comentario o /*comentario*/
+
+// 14.	¿Como se llama al siguiente ejemplo de funciones?
+  
+var myFunction = function(){
+  ...
+}
+/*Función expresiva
+
+15.	¿Como se le conoce a la siguiente sentencia?
+
+inicializar una variable
+
+16.	¿Qué significa Coerción en JS?
+
+Es el proceso de convertir el valor de un tipo a otro
+
+17.	¿Qué es el hoisting en JavaScript?
+
+Es cuando las declaraciones de variables y funciones se procesan antes de ejecutar cualquier código*/
+  
+  
+  
+   
