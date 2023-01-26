f43e307493d5574b17af5da18defce11d37a7182
Luis Fernando Montoya <66334502+FernandoMontoya25@users.noreply.github.com>
2023-01-26 14:42:20 -0600

    Create arrowFunction.js

---
+++ arrowFunction.js
@@ -0,0 +1,15 @@
+// Function normal
+const sumar2 = function(n1, n2) {
+    console.log(n1 + n2);
+}
+
+sumar2(5, 10);
+
+
+// arrow function
+const sumar1 = (n1, n2) => console.log(n1 + n2);
+sumar1(5, 10);
+
+const aprendiendo = (tecnologia) => console.log(`Aprendiendo ${tecnologia}`)
+
+aprendiendo('JavaScript');
